39bddd4c19cb64ae283f2180f98abe800c15cc27
SandeSydney <sandesydney01@gmail.com>
2022-11-17 05:16:55 +0300

    Login Component

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function Login() {
+  const [uname, setUname] = useState('')
+  const [pass, setPass] = useState('')
+  const [logStat, setLogStat] = useState('')
+
+  const unameRef = useRef(null)
+  const passRef = useRef(null)
+
+  const handleChange = ()=>{
+    setUname(unameRef.current.value)
+    setPass(passRef.current.value)
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    console.log(uname, pass);
+    if(uname.trim() !== '' && pass.trim() !== ''){
+
+    }
+    clearInput()
+  }
+
+  const clearInput = ()=>{
+    unameRef.current.value = ''
+    passRef.current.value = ''
+  }
+
   return (
-    <div>Login</div>
+    <div className="container">
+      <div className='loginNav'>
+        <h1>SanMart Shopping</h1>
+      </div>
+      <hr />
+      <div className='container'>
+        <div className='loginDiv'>
+          <form>
+            <div>
+              <h2>Login</h2>
+            </div>
+            <div className="form-element">
+              <label htmlFor="userName">Username:</label>
+              <input type="text" name='userName' ref={unameRef} onChange={handleChange}/>
+            </div>
+            <div className="form-element">
+              <label htmlFor="password">Password:</label>
+              <input type="text" name='password' ref={passRef} onChange={handleChange}/>
+            </div>
+            <div className="formBtns">
+              <button onClick={handleLogin}>Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 

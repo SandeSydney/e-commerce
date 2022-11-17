26db14c36f74bdc27ffdda2675d26a7c636ebd60
SandeSydney <sandesydney01@gmail.com>
2022-11-17 06:25:42 +0300

    Worked on navigation links and styling

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -4,10 +4,20 @@ import { Link } from 'react-router-dom'
 function Navigation() {
     return (
         <nav>
-            <h1>SanMart Shopping</h1>
+            <h1>
+                <Link className='linkItems' to={'/homepage'}>
+                    SanMart Shopping
+                </Link>
+            </h1>
+            <div className="navLinks">
+                <Link className='links' to={"/homepage/products"}>Products</Link>
+                <Link className='links' to={"/homepage/about"}>About</Link>
+                <Link className='links' to={"/homepage/contact"}>Contact</Link>
+                <Link className='links' to={"/homepage/cart"}>Shopping Cart</Link>
+            </div>
             <div className='schLogout'>
                 <div className="search">
-                    <input type="text" name='searchBar' placeholder='Search'/>
+                    <input type="text" name='searchBar' placeholder='Search' />
                 </div>
                 <div className="logout">
                     <Link to={"/logout"}>Logout</Link>

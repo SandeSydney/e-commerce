f17366a17ae9e6a7651112f6c264e101ddfd2a0b
SandeSydney <sandesydney01@gmail.com>
2022-11-21 04:10:02 +0300

    Products' component update

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import ProductItem from './ProductItem'
+import SideNavigation from './SideNavigation'
 
-function Products() {
+
+const Products = () => {
+  const products = useSelector((state) => state.products.value)
+
+  const renderedProducts = products.map((product, id) => {
+    return <ProductItem key={id} product={product} />
+  })
   return (
-    <div className='container'>Products Page</div>
+    <div className='products'>
+      <div className="sideNav">
+        <SideNavigation />
+      </div>
+      <div className='productsDisplay'>{renderedProducts}</div>
+    </div>
   )
 }
 

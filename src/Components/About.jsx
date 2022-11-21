2e7696089ef595cf7804a669c8c64a23e90e712e
SandeSydney <sandesydney01@gmail.com>
2022-11-21 08:56:14 +0300

    Sample Baout page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='container'>
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio totam aliquam magnam tempora cum animi iusto numquam ut laudantium, quis, suscipit distinctio vero delectus illo sed excepturi et eius itaque.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio totam aliquam magnam tempora cum animi iusto numquam ut laudantium, quis, suscipit distinctio vero delectus illo sed excepturi et eius itaque.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio totam aliquam magnam tempora cum animi iusto numquam ut laudantium, quis, suscipit distinctio vero delectus illo sed excepturi et eius itaque.
+        </p>
+    </div>
   )
 }
 

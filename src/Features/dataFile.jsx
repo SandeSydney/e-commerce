62c79b615baaf1a42e0a37ed8af5502222ac9992
SandeSydney <sandesydney01@gmail.com>
2022-11-21 08:59:34 +0300

    sample data

---
+++ src/Features/dataFile.jsx
@@ -0,0 +1,172 @@
+import React from 'react'
+
+export const dataFile = [
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    },
+    {
+        name:'Chair',
+        desc: 'massive brown chair',
+        price: '30',
+        img_url: 'http://www.chair.com/chair.jpg',
+        discount_rate: '2'
+    }
+]

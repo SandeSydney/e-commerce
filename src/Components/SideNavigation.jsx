4ddb1ac800c6abfe0d921fd824ad572e5c2aa711
SandeSydney <sandesydney01@gmail.com>
2022-11-21 08:56:56 +0300

    Side Navigation

--- src/Components/SideNavigation.jsx
+++ src/Components/SideNavigation.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function SideNavigation() {
+  const sort = ()=>{
+    alert('Hello Sande')
+  }
   return (
-    <div>SideNavigation</div>
+    <div className='sideNav'>
+      <Link className='sideNavLinks' onClick={sort}>Ascending Order</Link>
+      <Link className='sideNavLinks' to={'add-product'}>Add Product</Link>
+    </div>
   )
 }
 

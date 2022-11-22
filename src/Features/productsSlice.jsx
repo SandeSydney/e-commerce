c53d0dbf653bd1c33e379c0542d8b8e7e7bf1e88
SandeSydney <sandesydney01@gmail.com>
2022-11-22 21:50:10 +0300

    Added redux thunk

--- src/Features/productsSlice.jsx
+++ src/Features/productsSlice.jsx
@@ -2,17 +2,22 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
 import { dataFile } from './dataFile'
 
-// let data
-// const getFakeData = async () => {
-//     await axios.get('https://fakestoreapi.com/products')
-//         .then(res => data = res.data)
-// }
-// console.log(getFakeData())
+const PRODUCTS_URL = 'https://fakestoreapi.com/products'
 
 const initialState = {
-    value: dataFile,
-    loading: 'idle'
+    value: [],
+    status: 'idle',
+    error: null
 }
+
+const fetchProducts = createAsyncThunk('products/fetchProducts', async () => {
+    try {
+        const response = await axios.get(PRODUCTS_URL)
+        return response.data
+    } catch (err) {
+        return err.message
+    }
+})
 
 export const productsSlice = createSlice({
     name: 'products',
@@ -24,8 +29,27 @@ export const productsSlice = createSlice({
         deleteProduct: (state, action) => {
             return state.value.filter((item, i) => i !== action.payload.index)
         }
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(fetchProducts.pending, (state, action)=>{
+                state.status = 'loading'
+            })
+            .addCase(fetchProducts.fulfilled, (state, action)=>{
+                state.status = 'succeeded'
+                const loadedProducts = action.payload.map(product=>{
+                    return product
+                })
+
+                state.products.value = state.products.value.concat(loadedProducts)
+            })
+            .addCase(fetchProducts.rejected, (state, action)=>{
+                state.status = 'failed'
+                state.error = action.error.message
+            })
     }
 })
 
+export const selectAllProducts = (state) => state.products.value
 export const { addProduct, deleteProduct } = productsSlice.actions
 export default productsSlice.reducer

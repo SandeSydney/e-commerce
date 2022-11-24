0cf31e96e930790fd735e66163678cec886b9add
SandeSydney <sandesydney01@gmail.com>
2022-11-24 23:46:04 +0300

    Added conditional rendering

--- src/Components/SideNavigation.jsx
+++ src/Components/SideNavigation.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { getShowAddForm, showAddForm } from '../Features/productsSlice'
 
 function SideNavigation() {
-  const sort = ()=>{
-    alert('Hello Sande')
-  }
+  const showAdd = useSelector(getShowAddForm)
+  const dispatch = useDispatch()
+
   return (
     <div className='sideNav'>
-      <Link className='sideNavLinks' onClick={sort}>Ascending Order</Link>
-      <Link className='sideNavLinks' to={'add-product'}>Add Product</Link>
+      {showAdd ? '' : <button>
+        <Link className='sideNavLinks' to={'add-product'} onClick={() => dispatch(showAddForm(true))}>
+          Add Product
+        </Link>
+      </button>}
     </div>
   )
 }

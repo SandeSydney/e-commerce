e5975a6d9a85836c27b8134a2495051fb64e9c48
SandeSydney <sandesydney01@gmail.com>
2022-11-25 02:01:20 +0300

    Added properties to product item

--- src/Components/ProductItem.jsx
+++ src/Components/ProductItem.jsx
@@ -5,9 +5,10 @@ function ProductItem({ product }) {
     <div className='productItem'>
       <img src={product.image} alt="product" />
       <p>{product.title}</p>
-      <p>{product.price}</p>
+      <p>${product.price}</p>
       <p>{product.category}</p>
       <p>{product.discount_rate}</p>
+      <p>Rating: {product.rating.rate} | <span>Count:{product.rating.count}</span></p>
       <button className='addToCartBtn'>Add to Cart</button>
     </div>
   )

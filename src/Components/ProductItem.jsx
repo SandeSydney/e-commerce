085d21a1e073d0f3a781d3ee3c06eefdf5e9881b
SandeSydney <sandesydney01@gmail.com>
2022-11-25 23:06:38 +0300

    Customizing product item details

--- src/Components/ProductItem.jsx
+++ src/Components/ProductItem.jsx
@@ -3,12 +3,11 @@ import React from 'react'
 function ProductItem({ product }) {
   return (
     <div className='productItem'>
-      <img src={product.image} alt="product" />
-      <p>{product.title}</p>
-      <p>${product.price}</p>
-      <p>{product.category}</p>
-      <p>{product.discount_rate}</p>
-      <p>Rating: {product.rating.rate} | <span>Count:{product.rating.count}</span></p>
+      <img src={product.image_url} alt="Product Item" />
+      <p>{product.name}</p>
+      <p>Ksh. {product.price}</p>
+      <p>{product.description}</p>
+      <p>{product.discount_rate}% Off!</p>
       <button className='addToCartBtn'>Add to Cart</button>
     </div>
   )

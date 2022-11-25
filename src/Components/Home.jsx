2c5e1aa95ace092c6ac9732a945bd0266896bfbd
SandeSydney <sandesydney01@gmail.com>
2022-11-25 21:33:02 +0300

    Removed scroll to top

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 import Navigation from './Navigation'
-import ScrollToTop from './ScrollToTop'
 
 function Home() {
     const user = useSelector((state) => state.users.value)
@@ -12,7 +11,6 @@ function Home() {
             <div className="">
                 <Outlet />
             </div>
-            <ScrollToTop />
         </>
     )
 }
